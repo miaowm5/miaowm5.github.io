225906e1e9a5ccfd0f94f3f1a97d4ef24b62f798
Monkey Juice <miaowm5@users.noreply.github.com>
2015-11-16 19:11:12 +0800

    Update ErogeTrailers下载简易化.user.js

--- Greasemonkey/ErogeTrailers下载简易化.user.js
+++ Greasemonkey/ErogeTrailers下载简易化.user.js
@@ -2,7 +2,7 @@
 // @name         ErogeTrailers下载简易化
 // @namespace    http://miaowm5.github.io/
 // @version      0.1
-// @description  ErogeTrailers下载简易化，只显示视频源地址和视频名称
+// @description  ErogeTrailers下载简易化
 // @author       Miaowm5
 // @include      /^http://erogetrailers.com/video/(\d+)$/
 // @grant        none
@@ -11,12 +11,14 @@
 
 function check_ready(times){
     if ($('.et4player_source').text() != '-'){
+        if ($('.et4player_source').text() != 'YouTube') { return }
+        $('#video_area').remove()
         var text = '<br/><input style="font-size:3em;width:1000px" value="' + $('h2').text() + '.mp4"'
         text += 'onmouseover="this.focus();" onfocus="this.select();"/><br/><br/><br/>'
         text += '<input style="font-size:2em;width:800px" value="' + $('.et4player_source>a').attr('href')
         text += '" onmouseover="this.focus();" onfocus="this.select();"/><br/><br/>'
-        text += '<a target="_black" href="http://en.savefrom.net/">解析</a>'
-        $('body').html(text)
+        var url = 'http://en.savefrom.net/#url=' + $('.et4player_source>a').attr('href') + '&utm_source=userjs-chrome&utm_medium=extensions&utm_campaign=link_modifier'
+        top.location = url
     }
     else if ( times > 60){ console.log('time_out') }
     else{ 
@@ -24,8 +26,7 @@ function check_ready(times){
         setTimeout(function(){check_ready(times+1)} ,1000)
     }
 }
-$(function(){    
-    $('#video_area').remove()
+$(function(){
     check_ready(0)
 })
 
